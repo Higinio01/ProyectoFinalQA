8f07ca8c539cdb761dc056186ba816dec50e7a33
Higinio Santos <101358454+Higinio01@users.noreply.github.com>
2025-07-23 16:01:40 -0400

    Configurando Cucumber para funcionar con Junit5

--- src/test/java/org/example/CucumberTest.java
+++ src/test/java/org/example/CucumberTest.java
@@ -1,19 +1,21 @@
 package org.example;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,html:build/reports/cucumber-report.html")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.example.stepdefinitions")
 @SpringBootTest
-@RunWith(Cucumber.class)
-@CucumberOptions(
-    features = "src/test/resources/features",
-    glue = "org.example.stepdefinitions",
-    plugin = {"pretty", "html:build/reports/cucumber-report.html"},
-    monochrome = true
-)
 @ActiveProfiles("test")
 public class CucumberTest {
 }
